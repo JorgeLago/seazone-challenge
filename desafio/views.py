94459c04eaf2ae222fd116c7d5946b8fc67bdfd0
Jorge Lago <jorgelago92@gmail.com>
2022-11-22 00:57:26 -0300

    Atualizando Projeto, Classes e Dados

--- desafio/views.py
+++ desafio/views.py
@@ -2,28 +2,36 @@ from rest_framework import viewsets, filters
 from desafio.models import Imovel, Anuncio, Reserva
 from desafio.serializer import ImovelSerializer, AnunciosSerializer, ReservasSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ImovelViewSet(viewsets.ModelViewSet):
     """Exibindo todos os Imoveis"""
     queryset = Imovel.objects.all()
     serializer_class = ImovelSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     search_fields = ['imovel_id']
     ordering_fields = ['imovel_id']
 
-class AnunciosSerializer(viewsets.ModelViewSet):
+class AnunciosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os Anuncios"""
     queryset = Anuncio.objects.all()
     serializer_class = AnunciosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     search_fields = ['anuncio_id']
     ordering_fields = ['anuncio_id']
     http_method_names = ['get','post', 'put', 'path']
 
-class ReservasSerializer(viewsets.ModelViewSet):
+class ReservasViewSet(viewsets.ModelViewSet):
     """Exibindo todos as Reservas"""
     queryset = Reserva.objects.all()
     serializer_class = ReservasSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     search_fields = ['reserva_id']
     ordering_fields = ['reserva_id']
--- seazonechallenge/urls.py
+++ seazonechallenge/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from desafio.views import ImovelViewSet, AnunciosSerializer, ReservasSerializer
+from desafio.views import ImovelViewSet, AnunciosViewSet, ReservasViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('imoveis', ImovelViewSet, basename='imoveis')
-router.register('anuncios', AnunciosSerializer, basename='anuncios')
-router.register('reservas', ReservasSerializer, basename='reservas')
+router.register('anuncios', AnunciosViewSet, basename='anuncios')
+router.register('reservas', ReservasViewSet, basename='reservas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
